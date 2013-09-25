cd12b371889f176c02c22d260f41386b310b569b
sodino <sodino@qq.com>
2013-09-25 21:43:01 +0800

    简单保存下..

--- src/lab/opportunity/activity/MainActivity.java
+++ src/lab/opportunity/activity/MainActivity.java
@@ -1,22 +1,26 @@
 package lab.opportunity.activity;
 
 import lab.opportunity.R;
+import lab.opportunity.util.LogOut;
+import android.graphics.drawable.Animatable;
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import android.widget.ImageView;
 
 public class MainActivity extends BaseActivity {
-	
-	
+	private ImageView imgLoading;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		imgLoading = (ImageView)findViewById(R.id.imgLoading);
+		showLoadingDialog();
+		LogOut.out("test");
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	private void showLoadingDialog(){
+		Animatable anim = (Animatable)imgLoading.getDrawable();
+		if(anim.isRunning() == false){
+			anim.start();
+		}
 	}
 }
