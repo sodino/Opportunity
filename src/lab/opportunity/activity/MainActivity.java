bf669d78a16388a297b0bb102fbf1aebd8240bdc
sodino <sodino@qq.com>
2013-09-25 11:53:02 +0800

    提交工程结构

---
+++ src/lab/opportunity/activity/MainActivity.java
@@ -0,0 +1,22 @@
+package lab.opportunity.activity;
+
+import lab.opportunity.R;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends BaseActivity {
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+}
