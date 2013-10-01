481e8e9eaaa65b927908f67d8804ed858ee614e9
sodino <sodino@qq.com>
2013-10-01 11:52:32 +0800

    提交一点代码

--- src/lab/sodino/jobs/activity/MainActivity.java
+++ src/lab/sodino/jobs/activity/MainActivity.java
@@ -1,8 +1,13 @@
 package lab.sodino.jobs.activity;
 
+import java.io.UnsupportedEncodingException;
+
 import lab.sodino.jobs.R;
 import lab.sodino.jobs.activity.BaseActivity;
+import lab.sodino.jobs.app.JobsConstant;
+import lab.sodino.util.DownloadInfo;
 import lab.sodino.util.LogOut;
+import lab.sodino.util.NetworkUtil;
 import android.graphics.drawable.Animatable;
 import android.os.Bundle;
 import android.widget.ImageView;
@@ -15,7 +20,32 @@ public class MainActivity extends BaseActivity {
 		setContentView(R.layout.activity_main);
 		imgLoading = (ImageView)findViewById(R.id.imgLoading);
 		showLoadingDialog();
-		LogOut.out("test");
+		
+		new Thread(){
+			public void run(){
+				DownloadInfo info = new DownloadInfo();
+				info.urlOriginal = JobsConstant.JOBS_JSON_URL;
+				info.dataAction = DownloadInfo.ACTION_READ;
+				NetworkUtil.downloadByJava(MainActivity.this, info);
+				if(info.resultCode == NetworkUtil.DOWNLOAD_SUCCESS){
+					String detail = null;
+					if(info.respContentEncoding != null && info.respContentEncoding.length() > 0){
+						try {
+							detail = new String(info.data, info.respContentEncoding);
+						} catch (UnsupportedEncodingException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}	
+					}
+					if(detail == null){
+						detail = new String(info.data);
+					}
+					LogOut.out(MainActivity.this, "detail("+info.respContentEncoding+")[" + new String(info.data) +"]");
+				}else{
+					LogOut.out(MainActivity.this, "resultCode=" + info.resultCode +" err=" + info.errorDetail);
+				}
+			}
+		}.start();
 	}
 
 	private void showLoadingDialog(){
