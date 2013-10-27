c62f50216fde6f0385e2e44c6e85b99fb943719b
sodino <sodino@qq.com>
2013-10-27 21:17:26 +0800

    数据库，写了一点点儿...

--- src/lab/sodino/jobs/db/data/Esse.java
+++ src/lab/sodino/jobs/db/data/Esse.java
@@ -1,8 +1,57 @@
 package lab.sodino.jobs.db.data;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
 /**所有数据的父类
  * @author Sodino E-mail:sodino@qq.com
  * @version Time：2013年10月1日 下午5:07:22
  */
 public abstract class Esse {
 	long _id = -1;
+	
+	public long get_id(){
+		return _id;
+	}
+	
+	public void set_id(long id){
+		_id = id;
+	}
+	
+	/**以类名为数据库表名。*/
+	public String getTableName() {
+		return getClass().getSimpleName();
+	}
+	
+	public String toString(){
+		StringBuilder sb = new StringBuilder();
+		sb.append("_id=" + _id);
+		//如果是子类的话，getClass()为子类名，非Esse 
+		Class<?> cls = getClass();
+		while(cls != Esse.class){
+			Field[] fArr = cls.getDeclaredFields();
+			if(fArr == null){
+				break;
+			}
+			for(Field f : fArr){
+				if(Modifier.isStatic(f.getModifiers())){
+					continue;
+				}
+				if(f.isAccessible() == false){
+					f.setAccessible(true);
+				}
+				String name = f.getName();
+				try{
+					Object value = f.get(this);
+					sb.append(",");
+					sb.append(name);
+					sb.append("=");
+					sb.append(value);
+				}catch(Exception e){
+					e.printStackTrace();
+				}
+			}
+		}
+		return sb.toString();
+	}
 }
--- src/lab/sodino/jobs/db/data/Job.java
+++ src/lab/sodino/jobs/db/data/Job.java
@@ -1,9 +1,11 @@
 package lab.sodino.jobs.db.data;
+
 /**
  * @author Sodino E-mail:sodino@qq.com
  * @version Time：2013年10月15日 下午4:41:04
  */
 public class Job extends Esse {
+	@unique
 	public String name;
 	public String place;
 	public String category;
